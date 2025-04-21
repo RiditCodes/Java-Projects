a2b67258ee12f9d6d085470d7d42525560339527
RiditCodes <90456597+RiditCodes@users.noreply.github.com>
2025-04-21 19:41:15 +0530

    Add initial Java project files and configuration

---
+++ InputProg.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class InputProg{
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int number;
+        System.out.print("Enter a number: ");
+        number = sc.nextInt();
+        System.out.println("The entered no. is: " + number);
+
+        sc.close();
+    }
+}
---
+++ Variable.java
@@ -0,0 +1,7 @@
+public class Variable{
+    public static void main(String[] args){
+        int num = 20;
+
+        System.out.println("The value of num is: " + num);
+    }
+}
